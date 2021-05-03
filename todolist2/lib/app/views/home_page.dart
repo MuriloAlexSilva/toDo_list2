4cc0cca5294925d5cdc3330c8172e93a10c646d0
Murilo Silva <murilo.alex.silva@gmail.com>
2021-05-02 22:30:59 -0300

    inserindo o save,read,get

--- todolist2/lib/app/views/home_page.dart
+++ todolist2/lib/app/views/home_page.dart
@@ -1,6 +1,8 @@
-import 'dart:math';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -68,5 +70,28 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  //Coletar dados
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  //Salvar dados
+  Future<File> _saveData() async {
+    String data = jsonEncode(_tarefas);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  //Ler dados
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
